2ed18375cb791605d5a52f73435f9765dd710187
Patricio Pérez Pinto <pato.perez.pinto@gmail.com>
2018-08-07 12:59:51 -0400

    [ambas] clase 07 de agosto de 2018

---
+++ Ambas/PrimerProyecto/PrimerProyecto/Jugador.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerProyecto {
+    class Jugador {
+        private int id;
+        private String nombre;
+        private int dorsal;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Dorsal { get => dorsal; set => dorsal = value; }
+
+        public override string ToString() {
+            return nombre + " - " + dorsal;
+        }
+
+        /*public String GetNombre() {
+            return this.nombre;
+        }
+
+        public void PegarBalon() {
+            Console.WriteLine("GOL!");
+        }*/
+    }
+}
---
+++ Ambas/PrimerProyecto/PrimerProyecto/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerProyecto {
+    class Program {
+        static void Main(string[] args) {
+            /*Jugador vidal = new Jugador();
+
+            // Vidal
+            vidal.Id = 1;
+            vidal.Nombre = "Arturo Vidal";
+            vidal.Dorsal = 0;
+
+            Console.WriteLine(vidal);
+            */
+
+            List<Jugador> lista = new List<Jugador>();
+
+            int cont = 1;
+            Jugador j;
+            while (true) {
+                j = new Jugador();
+                Console.Write("Ingrese nombre: ");
+                j.Nombre = Console.ReadLine();
+
+                Console.Write("Ingrese dorsal: ");
+
+
+                if (cont == 3) {
+                    break;
+                }
+                cont++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
