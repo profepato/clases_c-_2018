2d517939ead520e6054cc94b4bfa07da9fe51b10
Patricio Pérez Pinto <pato.perez.pinto@gmail.com>
2018-08-07 15:45:08 -0400

    [sec2] clase 7 de agosto de 2018

---
+++ Ambas/App/App/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace App {
+    class Program {
+        static void Main(string[] args) {
+            Deporte futbol = new Deporte();
+            Deporte basket = new Deporte();
+
+            futbol.Id = 1;
+            futbol.Nombre = "Futbol";
+            futbol.Popularidad = 95;
+
+            basket = new Deporte() {
+                Popularidad = 60,
+                Id = 2,
+                Nombre = "Baskett"
+            };
+
+            Deporte d = new Deporte();
+
+            d.Id = 3;
+            Console.Write("Nombre deporte: ");
+            d.Nombre = Console.ReadLine();
+
+            Console.Write("Popularidad: ");
+            d.Popularidad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(d.Id);
+            Console.WriteLine(d.Nombre);
+            Console.WriteLine(d.Popularidad);
+
+
+            List<Deporte> deportes = new List<Deporte>();
+
+            deportes.Add(futbol);
+            deportes.Add(basket);
+            deportes.Add(d);
+
+            Console.WriteLine("Cantidad: "+deportes.Count);
+
+            foreach (Deporte dep in deportes) {
+                Console.WriteLine("{0} - {1} - {2}", 
+                    dep.Id, dep.Nombre, dep.Popularidad);
+            }
+
+            if (d.Popularidad > 50) {
+                Console.WriteLine(d.Nombre+" es popular");
+            }
+            
+
+
+            Console.Write("Presione una tecla...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ambas/App/Model/Deporte.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model {
+    public class Deporte {
+        private int id;
+        private String nombre;
+        private int popularidad;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Popularidad { get => popularidad; set => popularidad = value; }
+    }
+}
