e870a216942ab0331f86dbce310ecf8e3bf55dfc
Patricio Pérez Pinto <pato.perez.pinto@gmail.com>
2018-08-14 12:34:58 -0300

    [sec2] 14 de agosto de 2018

--- sec_2/Test_WF_140818/Test_WF_140818/Form1.Designer.cs
+++ sec_2/Test_WF_140818/Test_WF_140818/Form1.Designer.cs
@@ -52,7 +52,7 @@
             this.ayudaToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(340, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(361, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -97,7 +97,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(340, 268);
+            this.ClientSize = new System.Drawing.Size(361, 272);
             this.Controls.Add(this.btnMenu);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
